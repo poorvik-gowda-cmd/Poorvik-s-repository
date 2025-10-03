ffb6877f133f43092316107915e80a0b57d64dbe
poorvik-gowda-cmd <poorvik29@gmail.com>
2025-10-03 16:45:11 +0530

    Add files via upload

---
+++ quiz bot.py
@@ -0,0 +1,63 @@
+print("hello,welcome to space quizzz,lets explore space. ")
+input()
+print("please enter your name" )
+name=input()
+print("okay,",name,"ready to explore your space knowledge with simple questions")
+response=input()
+if (input=="no"or"n"):
+ print("come back again")
+else:
+    input()
+    print("you will be having three question to answer each question have 4 options")
+    print("each correct answer adds you up with 5 pts and incorrect answers take away 1 point from you")
+    print("if you get all 3 correct questions right ypu win great reward including trip to NASA")
+
+    print("question 1 is as  follows")
+    print("WHICH IS THE 5 PLANET IN OUR SOLAR SYSTEM ?")
+    print("1",'EARTH')
+    print("2",'MARS')
+    print("3","JUPITER")
+    print("4",'URANUS')
+
+    ANSWER_1=input("please enter the correct option number:" )
+    if (ANSWER_1=="3"or"JUPITER"or"jupiter"):
+        print("correct answer you got 5 points")
+    else:
+        print("oops!,that is incorrect you lost 1 point")
+        print("")
+    
+    input()
+    print("question 2 is as  follows")
+    print("WHICH GALAXY IS CLOSEST TO OUR MILKYWAY ?")
+    print("1",'ANDROMEDA')
+    print("2",'BLACK EYE')
+    print("3","CARTWHEEL")
+    print("4",'WHIRPOOL')
+
+    ANSWER_2=input("please enter the correct option number:" )
+    if (ANSWER_2=="1"or"ANDROMEDA"or"andromeda"):
+        print("correct answer you got 5 points")
+    else:
+        print("oops!,that is incorrect you lost 1 point")
+        print("")
+
+    input()
+    print("question 3 is as  follows")
+    print("HOW MANY MOONS DOES NEPTUNE HAVE?")
+    print("1",'15')
+    print("2",'16')
+    print("3","10")
+    print("4",'0')
+
+    ANSWER_3=input("please enter the correct option number:" )
+    if (ANSWER_3=="2"or"16"):
+        print("correct answer you got 5 points")
+    else:
+        print("oops!,that is incorrect you lost 1 point")
+    input()
+    
+    print("that's great you have sucessfully completed the quiz ,your response has beenrecorded and score has been calculated we will get back to you soon")
+
+
+    
+
