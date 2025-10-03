1c523676fda8e0259b13742fb25184ef7574b677
poorvik-gowda-cmd <poorvik29@gmail.com>
2025-10-03 14:35:16 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+x=input()
+print("hi do you mind telling me your good name")
+name=input()
+print("hello",name,"please give your contact number")
+number=input()
+if number.isdigit:
+    print("thank you.... ")
+else:
+    print("bye bye")
