80a4404609363cfcb8494894e7806ec31834df28
poorvik-gowda-cmd <poorvik29@gmail.com>
2025-10-04 11:51:09 +0530

    Update joke BOT.py

--- joke BOT.py
+++ joke BOT.py
@@ -1,8 +1,10 @@
 import requests
+#this code gives only 1 random joke at once
 
 url="https://official-joke-api.appspot.com/random_joke"
 response = requests.get(url)
 
 joke=response.json()
 print(joke["setup"])
+
 print(joke["punchline"])
