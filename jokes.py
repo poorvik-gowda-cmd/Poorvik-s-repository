d9ca1b05e5ee919b7471946ae624fbe48b7ea596
poorvik-gowda-cmd <poorvik29@gmail.com>
2025-10-04 11:53:04 +0530

    Update jokes.py

--- jokes.py
+++ jokes.py
@@ -1,8 +1,9 @@
-while True:
+while True:              #this file is capable of giving you as many jokes as you want in one run
     import requests
     url="https://official-joke-api.appspot.com/random_joke"
     response=requests.get(url)
     joke=response.json()
     print(joke["setup"])
     input()
+
     print(joke["punchline"])
